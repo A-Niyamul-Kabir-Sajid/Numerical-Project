456a3994e23f17531295fc9cd4be7b3806ca7fc3
akid2207018-wq <akid2207018@stud.kuet.ac.bd>
2025-12-18 00:03:25 +0600

    revised secant method

--- Solution of Non-Linear Equations/Secant method.cpp
+++ Solution of Non-Linear Equations/Secant method.cpp
@@ -1,125 +1,86 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
- 
-void printfunc(vector<int>cof)
-{
-    //reverse(cof.begin(),cof.end());
-    int n=cof.size();
-    for(int i=0; i<cof.size(); i++)
-    {
-        if(cof[i]>0&&i!=0)cout<<'+';
-        if(cof[i]!=0)
-        {
-            cout<<cof[i];
-            if(n-i-1>1)
-            {
-                cout<<"x^"<<n-i-1;
-            }
-            else if(i==n-1)
-            {
-                //cout<<cof[i]<<endl;
-            }
-            else
-            {
-                cout<<"x";
-                //cout<<endl;
-            }
+
+const double EPS = 1e-8;   // convergence tolerance
+const int MAX_ITER = 100;   // max iterations per root
+const double STEP = 0.5;    // interval step for root scanning
+
+// Print polynomial nicely
+void printfunc(vector<int> cof) {
+    int n = cof.size();
+    for(int i = 0; i < n; i++) {
+        if(cof[i] > 0 && i != 0) cout << '+';
+        if(cof[i] != 0) {
+            cout << cof[i];
+            if(n-i-1 > 1) cout << "x^" << n-i-1;
+            else if(n-i-1 == 1) cout << "x";
         }
- 
- 
     }
-    cout<<endl;
+    cout << endl;
 }
- 
-float func(vector<int>cof,float x)
-{
-    float ret=0;
-    float xp=1;
-    reverse(cof.begin(),cof.end());
- 
-    for(int i=0; i<cof.size(); i++)
-    {
-        float temp=xp*cof[i];
-        ret=ret+temp;
-        xp=xp*x;
-    }
+
+// Evaluate polynomial at x
+double func(const vector<int>& cof, double x) {
+    double ret = 0;
+    int n = cof.size();
+    for(int i = 0; i < n; i++)
+        ret = ret*x + cof[i];  // Horner's method
     return ret;
 }
-int main()
-{
-    //cout<<"Hello world\n";
+
+int main() {
     int n;
-    cin>>n;
-    vector<int>cof;
-    for(int i=0; i<=n; i++)
-    {
-        int x;
-        cin>>x;
-        cof.push_back(x);
-    }
+    cin >> n;
+
+    vector<int> cof(n+1);
+    for(int i = 0; i <= n; i++)
+        cin >> cof[i];
+
     printfunc(cof);
- 
-    float itstart=1000000.355;
- 
-    vector<float>vv;
-   /* for(float i=-itstart; i<itstart; i+=.5)
-    {
-        float it2=i+.5;
-        float f1=func(cof,i);
-        float f2=func(cof,it2);
-        if(f1*f2<0){
-            vv.push_back(i);
-        }
-    }*/
-    if(vv.size()==0){
-        vv.push_back(0);
-    }
-    else cout<<vv.size()<<endl;
- 
-    vector<float>res;
-    float E=.00001;
- 
-    for(int i=0;i<vv.size();i++){
-            float it=vv[i];
-            float it2=it+.5;
- 
-        cout<<"Iteration starts for :"<<it<< " "<<it2<<endl;
- 
-        int cnt=1;
-        map<float,bool>mp;
- 
-        while(1){
-                cout<<"Number of iteration :"<<cnt<<endl;
- 
-            //1,2
-            float f1=func(cof,it);
-            float f2=func(cof,it2);
-            // calculate 3
-            float xr=it2-f2*(it-it2)/(f1-f2);
-            cout<<it<<" "<<it2<<" "<<xr<<endl;
- 
-            float tmp=xr-it2;
-            if(tmp<0)tmp=-tmp;
-            tmp=tmp/xr;
- 
- 
-            if(((tmp-E)<0)||mp[xr]){
-                    res.push_back(xr);
+
+    vector<double> roots;
+    double start = -1000;  // scanning range
+    double end = 1000;
+
+    // scan intervals for sign changes
+    for(double i = start; i < end; i += STEP) {
+        double f1 = func(cof, i);
+        double f2 = func(cof, i + STEP);
+        if(f1*f2 <= 0) {  // possible root
+            double x0 = i, x1 = i+STEP;
+            int iter = 0;
+            set<long long> seen;  // to avoid duplicates
+
+            while(iter < MAX_ITER) {
+                double f0 = func(cof, x0);
+                double f1val = func(cof, x1);
+
+                if(fabs(f1val - f0) < EPS) break;  // avoid division by zero
+
+                double xr = x1 - f1val*(x0 - x1)/(f0 - f1val);
+
+                if(fabs(xr - x1) < EPS || seen.count(round(xr*1e6))) {
+                    roots.push_back(xr);
                     break;
- 
+                }
+
+                seen.insert(round(xr*1e6));
+                x0 = x1;
+                x1 = xr;
+                iter++;
             }
-            it=it2;
-            it2=xr;
-            mp[xr]=1;
-            cnt++;
- 
         }
     }
- 
-    cout<<"Results are : "<<endl;
-    for(auto i:res){
-        cout<<i<<" ";
+
+    if(roots.empty()) {
+        cout << "No real roots found.\n";
+    } else {
+        cout << "Roots found:\n";
+        sort(roots.begin(), roots.end());
+        for(double r : roots)
+            cout << fixed << setprecision(6) << r << " ";
+        cout << endl;
     }
-    cout<<endl;
- 
+
+    return 0;
 }
