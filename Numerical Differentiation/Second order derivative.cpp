ca49785366dd5b9da655d7e7d357fc0db1314492
ShahariarEmon112 <shahariaremon234@gmail.com>
2025-12-19 16:02:41 +0600

    Added Numerical Differentiation input output

---
+++ Numerical Differentiation/Second order derivative.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Polynomial evaluation
+double f(vector<double>& a, double x)
+{
+    double s = 0;
+    for(int i = 0; i < a.size(); i++)
+        s += a[i] * pow(x, i);
+    return s;
+}
+int main()
+{
+    int degree;
+    //cout << "Enter degree of polynomial: ";
+    cin >> degree;
+    vector<double> a(degree + 1);
+    //cout << "Enter coefficients a_n to a_0:\n";
+    for(int i = degree; i >= 0; i--)
+        cin >> a[i];
+    double p;
+    //cout << "Enter value of p: ";
+    cin >> p;
+    double h = 1.0;
+    double fpp = (f(a, p + h) - 2*f(a, p) + f(a, p - h)) / (h * h);
+    cout << "f\"(p) = " << fpp << endl;
+
+    return 0;
+}
+/*
+Input:
+3
+1 0 -1 1
+2
+Output:
+f"(p) = 12
+*/
